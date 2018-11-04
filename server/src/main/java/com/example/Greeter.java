07cd4eae240279f48a8eca1b025978b769be5465
abdennour <mail@abdennoor.com>
2018-11-04 13:56:25 +0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /**
-   * This is the main method
+   * This is the main method.
    */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
